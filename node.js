5766a5ea40f6441b2b50e97b9da6eefd16b6c6fd
egy186 <egy186@yahoo.co.jp>
2017-03-15 05:34:28 +0900

    add eslint-plugin-node

--- node.js
+++ node.js
@@ -1,3 +1,18 @@
 'use strict';
 
-module.exports = { env: { node: true } };
+module.exports = {
+  env: { node: true },
+  plugins: ['node'],
+  rules: {
+    'node/exports-style': 'error',
+    'node/no-deprecated-api': 'error',
+    'node/no-missing-import': 'error',
+    'node/no-missing-require': 'error',
+    'node/no-unpublished-bin': 'error',
+    'node/no-unpublished-import': 'error',
+    'node/no-unpublished-require': 'error',
+    'node/no-unsupported-features': 'error',
+    'node/process-exit-as-throw': 'error',
+    'node/shebang': 'error'
+  }
+};
