a40dfa55a0672473968cd7e5e08b5c144a35331b
egy186 <egy186@yahoo.co.jp>
2018-12-09 18:14:29 +0900

    add jsdoc linting

--- index.js
+++ index.js
@@ -3,5 +3,6 @@
 const _ = require('lodash');
 const base = require('./base');
 const es2015 = require('./es2015');
+const jsdoc = require('./jsdoc');
 
-module.exports = _.merge(base, es2015);
+module.exports = _.merge(base, es2015, jsdoc);
---
+++ jsdoc.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  plugins: ['jsdoc'],
+  rules: {
+    'jsdoc/check-param-names': 'error',
+    'jsdoc/check-tag-names': 'error',
+    'jsdoc/check-types': 'error',
+    'jsdoc/newline-after-description': 'error',
+    'jsdoc/no-undefined-types': 'error',
+    'jsdoc/require-description': 'off',
+    'jsdoc/require-description-complete-sentence': 'error',
+    'jsdoc/require-example': 'off',
+    'jsdoc/require-hyphen-before-param-description': 'error',
+    'jsdoc/require-param': 'error',
+    'jsdoc/require-param-description': 'error',
+    'jsdoc/require-param-name': 'error',
+    'jsdoc/require-param-type': 'error',
+    'jsdoc/require-returns-description': 'error',
+    'jsdoc/require-returns-type': 'error',
+    'jsdoc/valid-types': 'error'
+  }
+};
