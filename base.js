8603a01cfaf1a318a3f4ebf96dc9878e96a5baf5
egy186 <egy186@yahoo.co.jp>
2018-12-25 00:17:05 +0900

    update eslint to version 5.11.0

--- base.js
+++ base.js
@@ -108,6 +108,7 @@ module.exports = {
     'no-unused-expressions': 'error',
     'no-unused-labels': 'off',
     'no-useless-call': 'error',
+    'no-useless-catch': 'error',
     'no-useless-concat': 'error',
     'no-useless-escape': 'error',
     'no-useless-return': 'error',
