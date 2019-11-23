d32c3d1ad659ae36851d3282d00c0bc79bc77b7e
egy186 <egy186@yahoo.co.jp>
2019-11-23 14:11:27 +0900

    update eslint to version 6.7.0

--- es2018.js
+++ es2018.js
@@ -3,6 +3,7 @@
 module.exports = {
   parserOptions: { ecmaVersion: 2018 },
   rules: {
+    'prefer-exponentiation-operator': 'error',
     'prefer-named-capture-group': 'error',
     'prefer-object-spread': 'error'
   }
