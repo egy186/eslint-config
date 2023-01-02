b851d4549d84a7a91f7ae7794e792daea5eba03e
egy186 <egy186@yahoo.co.jp>
2023-01-02 22:42:40 +0900

    chore: update eslint-plugin-jest to version 27.2.0

--- jest.js
+++ jest.js
@@ -29,6 +29,7 @@ module.exports = {
     'jest/no-standalone-expect': 'error',
     'jest/no-test-prefixes': 'error',
     'jest/no-test-return-statement': 'error',
+    'jest/no-untyped-mock-factory': 'error',
     'jest/prefer-called-with': 'error',
     'jest/prefer-comparison-matcher': 'error',
     'jest/prefer-each': 'error',
