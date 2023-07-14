7db3922d9a251987514ee71d84b910b239d31550
egy186 <egy186@yahoo.co.jp>
2023-07-15 01:25:15 +0900

    feat: update @typescript-eslint/eslint-plugin to version 6.0.0

--- typescript.js
+++ typescript.js
@@ -126,7 +126,6 @@ module.exports = {
     '@typescript-eslint/no-shadow': 'error',
     '@typescript-eslint/no-this-alias': 'error',
     '@typescript-eslint/no-throw-literal': 'error',
-    '@typescript-eslint/no-type-alias': ['error', { allowAliases: 'in-unions-and-intersections' }],
     '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
     '@typescript-eslint/no-unnecessary-condition': 'error',
     '@typescript-eslint/no-unnecessary-qualifier': 'error',
