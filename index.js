94b29feb61adeae3a57f49352d3b6c88dd553082
egy186 <egy186@gmail.com>
2023-11-11 21:38:25 +0900

    fix: concat arrays

--- index.js
+++ index.js
@@ -5,4 +5,9 @@ const base = require('./base');
 const jsdoc = require('./jsdoc');
 const stylistic = require('./stylistic');
 
-module.exports = _.merge(base, jsdoc, stylistic);
+module.exports = _.mergeWith(base, jsdoc, stylistic, (objValue, srcValue) => {
+  if (Array.isArray(objValue)) {
+    return objValue.concat(srcValue);
+  }
+  return undefined;
+});
