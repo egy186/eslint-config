d0f0f23e35d48ad6baeabf20394784aad83c63c5
egy186 <egy186@gmail.com>
2025-01-14 22:15:13 +0900

    feat: update typescript-eslint to version 8.20.0

--- src/typescript.ts
+++ src/typescript.ts
@@ -102,6 +102,7 @@ const config = {
     '@typescript-eslint/no-meaningless-void-operator': 'error',
     '@typescript-eslint/no-misused-new': 'error',
     '@typescript-eslint/no-misused-promises': 'error',
+    '@typescript-eslint/no-misused-spread': 'error',
     '@typescript-eslint/no-mixed-enums': 'warn',
     '@typescript-eslint/no-namespace': 'error',
     '@typescript-eslint/no-non-null-asserted-nullish-coalescing': 'error',
